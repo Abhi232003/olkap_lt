4304013c1d886ddd59a602ef54419244dc7394dc
Prathamesh Mudgale <prathameshmudgale@gmail.com>
2024-11-27 23:31:06 +0530

    bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Hero from './Components/hero/hero'
 import Navbar from './Components/navbar/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
